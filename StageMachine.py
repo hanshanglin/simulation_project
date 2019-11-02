bdaf794595aa740e8ee84d6e574c2474f3c85d6f
hanshanglin <hanshanglin2017@gmail.com>
2019-11-02 12:50:17 +0800

    fix typo

--- StageMachine.py
+++ StageMachine.py
@@ -1,29 +1,28 @@
-import numpy as np
 import Entity
-from S_State import machine
+from S_State import machine_state
 class StageMachine(object):
     def __init__(self, RV_generater, machine_type):
         self.RV = RV_generater
         self.type = machine_type
-        self.state = machine.Idle
+        self.state = machine_state.Idle
         self.next_idle_time = -1
         self.idle_time = 0
         self.entity = None
         
     def update(self,cur_time):
-        if self.state == machine.Idle:
+        if self.state == machine_state.Idle:
             self.idle_time +=1
         else:
             if cur_time>=self.next_idle_time:
-                self.state = machine.Idle
+                self.state = machine_state.Idle
                 self.entity.finish(cur_time)
                 self.entity = None
         return self.state
     
     def release(self,cur_time):
-        if self.state == machine.Work:
+        if self.state == machine_state.Work:
             if cur_time>=self.next_idle_time:
-                self.state = machine.Idle
+                self.state = machine_state.Idle
                 self.entity.finish(cur_time)
                 self.entity = None
         return self.state
@@ -33,14 +32,14 @@ class StageMachine(object):
             print("ERROR:TYPE UNMATCHED")
             exit(1)
         if(not self.isIdle):
-            print("ERROR:entity double pocessed")
+            print("ERROR:entity double processed")
         rv = self.RV()
         self.next_idle_time = cur_time+rv
-        self.state = machine.Work
+        self.state = machine_state.Work
         self.entity = entity
         self.entity.start(cur_time)
         return 0
 
     def isIdle(self):
-        return self.state == machine.Idle
+        return self.state == machine_state.Idle
 
