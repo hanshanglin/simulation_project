2839bfc451b28ffea02dfbbeba8f56b1e14dfa6d
hanshanglin <hanshanglin2017@gmail.com>
2019-11-02 12:38:53 +0800

    add note and next() function

--- S_State.py
+++ S_State.py
@@ -2,13 +2,19 @@ from enum import Enum, unique
 
 
 @unique
-class machine(Enum):
+class machine_state(Enum):
     Idle = 0  
     Work = 1
 
+    def next(self):
+        cls = self.__class__
+        members = list(cls)
+        index = 1-members.index(self)
+        return members[index]
+
 
 @unique
-class entity(Enum):
+class entity_state(Enum):
     Spinning = 1
     Weaving = 2
     Finishing = 3
@@ -20,5 +26,5 @@ class entity(Enum):
         members = list(cls)
         index = members.index(self) + 1
         if index >= len(members):
-            index = 0
+            index = index - 1
         return members[index]
