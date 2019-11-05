4f1e12188c1849acc0806badbceff84827267925
hanshanglin <hanshanglin2017@gmail.com>
2019-11-05 17:37:03 +0800

    add entity & machine id

--- Entity.py
+++ Entity.py
@@ -1,18 +1,25 @@
 from S_State import entity_state
-class Entity(object):
+
+class entity_count():
+    count = 0
+class Entity(entity_count):
     def __init__(self):
         self.state = entity_state.Spinning
         self.start_time = []
         self.end_time = []
         self.processing = False
+        self.id = entity_count.count
+        entity_count.count +=1
 
     def finish(self,cur_time):
         self.state = self.state.next()
         self.end_time.append(cur_time)
+        self.processing = not self.processing
         return self.state
     
     def start(self,cur_time):
         self.start_time.append(cur_time)
+        self.processing = not self.processing
         return self.state
 
     def stage(self):
@@ -20,5 +27,8 @@ class Entity(object):
 
     def isProcessing(self):
         return self.processing
+
+    def getName(self):
+        return "entity-"+str(self.id)
 
 
--- StageMachine.py
+++ StageMachine.py
@@ -1,6 +1,14 @@
 import Entity
 from S_State import machine_state
-class StageMachine(object):
+import logging
+from config import *
+
+logging.basicConfig(format='[line:%(lineno)d] - %(levelname)s: %(message)s',
+                    level=OUTPUTLEVEL)
+class machine_count():
+    count = 0
+class StageMachine(machine_count):
+    machine_id = 0
     def __init__(self, RV_generater, machine_type):
         self.RV = RV_generater
         self.type = machine_type
@@ -8,14 +16,17 @@ class StageMachine(object):
         self.next_idle_time = -1
         self.idle_time = 0
         self.entity = None
+        self.id = machine_count.count 
+        machine_count.count += 1
         
     def update(self,cur_time):
         if self.state == machine_state.Idle:
             self.idle_time +=1
         else:
             if cur_time>=self.next_idle_time:
-                self.state = machine_state.Idle
+                logging.debug(str(self.type)+str(self.id)+" finish: "+self.entity.id)
                 self.entity.finish(cur_time)
+                self.state = machine_state.Idle
                 self.entity = None
         return self.state
     
@@ -23,9 +34,13 @@ class StageMachine(object):
         if self.state == machine_state.Work:
             if cur_time>=self.next_idle_time:
                 self.state = machine_state.Idle
+                logging.debug("{:<20}          finish{:>15}".format(self.getName(),self.entity.getName()))
                 self.entity.finish(cur_time)
                 self.entity = None
-        return self.state
+                return INTMAX
+            else:
+                return self.next_idle_time
+        return INTMAX
 
     def process(self,entity,cur_time):
         if(self.type != entity.state.next()):
@@ -38,8 +53,11 @@ class StageMachine(object):
         self.state = machine_state.Work
         self.entity = entity
         self.entity.start(cur_time)
+        logging.debug("{:<20}start processing{:>15} rv:{:>5} next:{:>10}".format(self.getName(),self.entity.getName(),str(rv),str(self.next_idle_time)))
         return 0
 
     def isIdle(self):
         return self.state == machine_state.Idle
 
+    def getName(self):
+        return str(self.type.name)+"-"+str(self.id)
